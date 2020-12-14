a95e5bfc14de28f589aa21fe8b0256764abd5b6f
Juan Pablo Ramírez Mendoza <jpmezzara@me.com>
2020-12-13 23:08:12 -0600

    unused generated classes

--- src/main/java/com/finkok/facturacion/stamp/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://facturacion.finkok.com/stamp", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.finkok.facturacion.stamp;
